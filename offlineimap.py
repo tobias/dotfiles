8915df344c4dac2b538f253c9646c80d5f3e00fd
Toby Crawley <toby@tcrawley.org>
2012-12-23 00:02:42 -0500

    Add offlineimap config.

---
+++ offlineimap.py
@@ -0,0 +1,12 @@
+#based on http://www.emacswiki.org/emacs/OfflineIMAP
+import subprocess
+def get_output(cmd):
+    # Bunch of boilerplate to catch the output of a command:
+    pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    (output, errout) = pipe.communicate()
+    assert pipe.returncode == 0 and not errout
+    return output
+
+def get_password_emacs(host, port):
+    cmd = "emacsclient --eval '(tc/get-auth-password \"%s\" \"%s\")'" % (host,port)
+    return get_output(cmd).strip().lstrip('"').rstrip('"')
